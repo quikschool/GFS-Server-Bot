27958b7cd9633b8797e9d5a4bf3162101dd3d66c
quikschool <79000571+quikschool@users.noreply.github.com>
2021-02-13 13:29:25 +0400

    Add files via upload

--- commands/1help.js
+++ commands/1help.js
@@ -1,35 +1,16 @@
 const loadCommands = require('./load-commands');
 const { prefix } = require('../config.json');
+const Discord = require('discord.js');
 
 module.exports = {
     commands: ['help'],
     description: 'A very helpful command to help you use this bot',
     callback: (message) => {
-        let reply = 'Why hello there! Here are my commands: \n\n\n ';
+        let reply = ['Why hello there! Here are my commands: \n\n\n '];
 
         const commands = loadCommands();
 
         for (const command of commands) {
-            let permissions = command.permission;
-
-            if (permissions) {
-                let hasPermission  = true;
-                if (typeof permissions === 'string') {
-                    permissions = [permissions];
-                }
-
-                for (const permission of permissions) {
-                    if (!message.member.hasPermission(permission)) {
-                        hasPermission = false;
-                        break;
-                    }
-                }
-
-                if (!hasPermission) {
-                    continue;
-                }
-            }
-
             const mainCommand = 
                 typeof command.commands === 'string' 
                     ? command.commands 
@@ -37,9 +18,39 @@ module.exports = {
             const args = command.expectedArgs ? ` ${command.expectedArgs}` : '';
             const { description } = command;
 
-            reply += `**${prefix}${mainCommand}${args}**: \n${description}\n\n`;
+            reply.push(`**${prefix}${mainCommand}${args}**: \n${description}\n\n`);
         }
 
-        message.channel.send(reply);
+        embed = new Discord.MessageEmbed()
+            .setTitle(reply[0])
+            .setColor('#FF0000')
+            .addFields(
+                {
+                    name: 'Help',
+                    value: reply[1]
+                },
+                {
+                    name: 'Ping',
+                    value: reply[2]
+                },
+                {
+                    name: 'Add tag',
+                    value: reply[3]
+                },
+                {
+                    name: 'Read tag',
+                    value: reply[4]
+                },
+                {
+                    name: 'Delete tag',
+                    value: reply[5]
+                },
+                {
+                    name: 'Mod delete tag',
+                    value: reply[6]
+                }
+            );
+
+        message.channel.send(embed);
     }
 }
